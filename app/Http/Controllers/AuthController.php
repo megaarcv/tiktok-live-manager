2737e6c694bfcaa33291f381fb94eed991bdc7c8
megaarcv <modalmega91@gmail.com>
2026-07-17 14:30:38 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -44,7 +44,11 @@ class AuthController extends Controller
         $data = $request->validate([
             'name'     => ['required', 'string', 'max:255'],
             'email'    => ['required', 'email', 'unique:users,email'],
-            'password' => ['required', 'confirmed', Password::min(8)],
+            'password' => ['required', 'confirmed', Password::min(8)
+    ->letters()
+    ->mixedCase()
+    ->numbers()
+    ->symbols()],
         ]);
 
         $user = User::create([
